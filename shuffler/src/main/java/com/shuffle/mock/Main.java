4026a1bc353d8b00cfb79ea479f0e60a65d09a5e
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2016-02-05 10:42:08 -0600

    Readme updated.

--- shuffler/src/main/java/com/shuffle/mock/Main.java
+++ shuffler/src/main/java/com/shuffle/mock/Main.java
@@ -1,5 +1,10 @@
 package com.shuffle.mock;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Implementation using mock addresses.
  *
@@ -9,7 +14,22 @@ public class Main {
 
     private Main() {}
 
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
     public static void main(String args) {
+        String fileName = "config.json";
+
+        try {
+            String content = readFile(fileName, Charset.defaultCharset());
+        } catch (IOException e) {
+            return;
+        }
+
 
     }
 }
