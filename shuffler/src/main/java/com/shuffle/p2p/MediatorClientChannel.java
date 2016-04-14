8051f9600263167a24ca06237b988168645e5c53
Eugene <elzeigel@gmail.com>
2016-04-13 23:27:28 -0400

    Envelope class, initial Payload coding

--- shuffler/src/main/java/com/shuffle/p2p/MediatorClientChannel.java
+++ shuffler/src/main/java/com/shuffle/p2p/MediatorClientChannel.java
@@ -4,22 +4,54 @@ package com.shuffle.p2p;
  * Created by Eugene Siegel on 4/12/16.
  */
 
-public class MediatorClientChannel<Identity, Payload> implements Channel<Identity, Payload> {
+public class MediatorClientChannel<Name, Address, Payload> implements Channel<Name, Payload> {
 
+    public class Envelope {
+        private Name to;
+        private Name from;
+        private Payload payload;
 
-    public MediatorClientChannel() {
+        public void setTo(Name to) {
+            this.to = to;
+        }
 
+        public void setFrom(Name from) {
+            this.from = from;
+        }
+
+        public void setPayload(Payload payload) {
+            this.payload = payload;
+        }
     }
 
-    public Channel<Identity, Payload> getChannel(Session<Identity,{to,from,payload}> session) {
+    public MediatorClientChannel(final Session<Address, Envelope> session) {
 
+
+        Channel<Name, Payload> c1 = new Channel<Name, Payload>() {
+            @Override
+            public Peer<Name, Payload> getPeer(Name you) {
+                return session.peer(); // incompatible types
+                //return null;
+            }
+
+            @Override
+            public Connection<Name, Payload> open(Listener<Name, Payload> listener) {
+                return null;
+            }
+        };
+        // return Channel<Name,Payload>
     }
 
-    public Peer<Identity,Payload> getPeer(Identity you) {
+    /*
+    public Channel<Identity, Payload> getChannel(Session<Identity,Payload> session) {
+        return null;
+    }*/
+
+    public Peer<Name,Payload> getPeer(Name you) {
         return null;
     }
 
-    public Connection<Identity, Payload> open(final Listener<Identity, Payload> listener) {
+    public Connection<Name, Payload> open(final Listener<Name, Payload> listener) {
         return null;
     }
 
