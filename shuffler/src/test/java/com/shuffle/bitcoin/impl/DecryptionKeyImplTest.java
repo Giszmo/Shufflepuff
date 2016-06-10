fc56dea5c7f353bb560d50953cbf07abaf5190e1
Leo Wandersleb <leo@leowandersleb.de>
2016-06-10 00:29:07 -0400

    some working ECIES crypto using spongycastle
    caveat: spongycastle is only for Android, while bouncycastle is not for Android. Spongycastle is not "signed by a JCA Code Singning Certification Authority" cause on Android nobody would care. Sun-Java on the other hand does care. For years it was considered a good idea to just stick with Sun-Java to avoid weird issues but here in this case, openjdk also skipps the certificate test, so I recommend to use openJDK for now or find the switch to disable the signature check.

--- shuffler/src/test/java/com/shuffle/bitcoin/impl/DecryptionKeyImplTest.java
+++ shuffler/src/test/java/com/shuffle/bitcoin/impl/DecryptionKeyImplTest.java
@@ -7,12 +7,21 @@ import com.shuffle.bitcoin.EncryptionKey;
 import org.bitcoinj.core.ECKey;
 import org.junit.Before;
 import org.junit.Test;
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+import org.spongycastle.util.encoders.Hex;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.security.Provider;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.spec.ECGenParameterSpec;
 import java.util.Arrays;
 import java.util.Enumeration;
+
+import javax.crypto.Cipher;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,69 +29,98 @@ import static org.junit.Assert.assertEquals;
  * Created by conta on 02.06.16.
  */
 public class DecryptionKeyImplTest {
+    static {
+        // add instance of provider class
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
+    }
 
-   ECKey ecKey;
-   BitcoinCrypto bitcoinCrypto;
-   DecryptionKey decryptionKey;
-   SecureRandom secureRandom;
-   EncryptionKey encryptionKey;
+    ECKey ecKey;
+    BitcoinCrypto bitcoinCrypto;
+    DecryptionKey decryptionKey;
+    SecureRandom secureRandom;
+    EncryptionKey encryptionKey;
 
-   @Before
-   public void setUp() throws Exception {
-      this.bitcoinCrypto = new BitcoinCrypto();
-      this.secureRandom = new SecureRandom();
-      this.ecKey = new ECKey(secureRandom);
-      this.decryptionKey = new DecryptionKeyImpl(this.ecKey);
-      //this.hKey = ECKey.fromPrivate()
+    @Before
+    public void setUp() throws Exception {
+        this.bitcoinCrypto = new BitcoinCrypto();
+        this.secureRandom = new SecureRandom();
+        this.ecKey = new ECKey(secureRandom);
+        this.decryptionKey = new DecryptionKeyImpl(this.ecKey);
+        //this.hKey = ECKey.fromPrivate()
 
-   }
+    }
 
+    @Test
+    public void testECIESAvailability() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDH", BouncyCastleProvider.PROVIDER_NAME);
+        keyPairGenerator.initialize(new ECGenParameterSpec("secp256r1"));
 
-   @Test
-   public void testToString() throws Exception {
-      System.out.println("\nBegin Test toString:");
-      String string = this.ecKey.getPrivateKeyAsWiF(bitcoinCrypto.getParams());
-      System.out.println("ECKey: " + this.ecKey);
-      System.out.println("String ECKey WIF: " + string);
-      System.out.println("String DecryptionKey: " + this.decryptionKey.toString());
-      assertEquals("toString Method: ", string, this.decryptionKey.toString());
+        KeyPair recipientKeyPair = keyPairGenerator.generateKeyPair();
+        PublicKey pubKey = recipientKeyPair.getPublic();
+        PrivateKey privKey = recipientKeyPair.getPrivate();
 
-   }
+        // init the encryption cipher
+        Cipher iesCipher = Cipher.getInstance("ECIES", BouncyCastleProvider.PROVIDER_NAME);
+        iesCipher.init(Cipher.ENCRYPT_MODE, pubKey);
 
-   @Test
-   public void testEncryptionKey() throws Exception {
+        // use the cipher
+        String message = "Hello ECIES World!";
+        byte[] encryptedMessage = iesCipher.doFinal(message.getBytes());
 
-      System.out.println("\nBegin Test encryptionKey:");
-      byte[] pub = ECKey.publicKeyFromPrivate(ecKey.getPrivKey(), ecKey.isCompressed());
-      EncryptionKey encryptionKey1 = new EncryptionKeyImpl(ECKey.fromPublicOnly(ecKey.getPubKey()));
+        // init the decryption cipher
+        iesCipher.init(Cipher.DECRYPT_MODE, privKey);
+        String decryptedMessage = new String(iesCipher.doFinal(encryptedMessage));
+
+        System.out.println(message);
+        System.out.println(" -> " + Hex.toHexString(encryptedMessage));
+        System.out.println(" -> " + decryptedMessage);
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        System.out.println("\nBegin Test toString:");
+        String string = this.ecKey.getPrivateKeyAsWiF(bitcoinCrypto.getParams());
+        System.out.println("ECKey: " + this.ecKey);
+        System.out.println("String ECKey WIF: " + string);
+        System.out.println("String DecryptionKey: " + this.decryptionKey.toString());
+        assertEquals("toString Method: ", string, this.decryptionKey.toString());
+
+    }
+
+    @Test
+    public void testEncryptionKey() throws Exception {
+
+        System.out.println("\nBegin Test encryptionKey:");
+        byte[] pub = ECKey.publicKeyFromPrivate(ecKey.getPrivKey(), ecKey.isCompressed());
+        EncryptionKey encryptionKey1 = new EncryptionKeyImpl(ECKey.fromPublicOnly(ecKey.getPubKey()));
 //
 //      PublicKey publicKey = BitcoinCrypto.loadPublicKey(Base64.getEncoder().encodeToString(ecKey.getPubKey()));
-      Provider p[] = Security.getProviders();
-      for (Provider aP : p) {
-         System.out.println(aP);
-         for (Enumeration e = aP.keys(); e.hasMoreElements(); )
-            System.out.println("\t" + e.nextElement());
-      }
-      System.out.println("ecKey: " + ecKey.toString());
-      System.out.println("ecKey priv: " + ecKey.getPrivateKeyAsHex());
-      System.out.println("secureRandom: " + secureRandom.toString());
-      System.out.println("decryptionKey: " + decryptionKey.toString());
-      System.out.println("ASN.1  " + Arrays.toString(ecKey.toASN1()));
+        Provider p[] = Security.getProviders();
+        for (Provider aP : p) {
+            System.out.println(aP);
+            for (Enumeration e = aP.keys(); e.hasMoreElements(); )
+                System.out.println("\t" + e.nextElement());
+        }
+        System.out.println("ecKey: " + ecKey.toString());
+        System.out.println("ecKey priv: " + ecKey.getPrivateKeyAsHex());
+        System.out.println("secureRandom: " + secureRandom.toString());
+        System.out.println("decryptionKey: " + decryptionKey.toString());
+        System.out.println("ASN.1  " + Arrays.toString(ecKey.toASN1()));
 
-      EncryptionKeyImpl encTest = new EncryptionKeyImpl(pub);
-      System.out.println("\nencTest: " + encTest);
-      System.out.println("encryptionKey: " + encryptionKey1);
-      System.out.println("EncKey.toString from ECKeys Pub: " + encTest.toString());
-      System.out.println("EncKey from DecKey to string: " + decryptionKey.EncryptionKey().toString());
+        EncryptionKeyImpl encTest = new EncryptionKeyImpl(pub);
+        System.out.println("\nencTest: " + encTest);
+        System.out.println("encryptionKey: " + encryptionKey1);
+        System.out.println("EncKey.toString from ECKeys Pub: " + encTest.toString());
+        System.out.println("EncKey from DecKey to string: " + decryptionKey.EncryptionKey().toString());
 
-      encryptionKey = new EncryptionKeyImpl(ecKey.getPubKey());
-      assertEquals(encryptionKey.toString(), decryptionKey.EncryptionKey().toString());
+        encryptionKey = new EncryptionKeyImpl(ecKey.getPubKey());
+        assertEquals(encryptionKey.toString(), decryptionKey.EncryptionKey().toString());
 
-   }
+    }
 
-   @Test
-   public void testDecrypt() throws Exception {
+    @Test
+    public void testDecrypt() throws Exception {
 
 
-   }
+    }
 }
